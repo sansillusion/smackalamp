59e84967da7fd2d1573c06b415e9b0889c2bdbff
Steve Olmstead <sansillusion@gmail.com>
2018-09-25 13:58:01 -0400

    Added random colors, more FX and random delays

--- smackalamp.ino
+++ smackalamp.ino
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////////////////
 //
-// Based on ALA library e
+// Based on ALA library
 //
 // Web page: http://yaab-arduino.blogspot.com
 //
@@ -27,6 +27,20 @@ AlaColor verts_[2] = { 0x00FF00, 0x00FF00 };
 AlaPalette verts = { 2, verts_ };
 AlaColor mauves_[2] = { 0xFF00FF, 0xFF00FF };
 AlaPalette mauves = { 2, mauves_ };
+AlaColor crazyb_[29] = { 0x000011, 0x000022, 0x000033, 0x000044, 0x000055, 0x000066, 0x000077, 0x000088, 0x000099, 0x0000AA, 0x0000BB, 0x0000CC, 0x0000DD, 0x0000EE, 0x0000FF, 0x0000EE, 0x0000DD, 0x0000CC, 0x0000BB, 0x0000AA, 0x000099, 0x000088, 0x000077, 0x000066, 0x000055, 0x000044, 0x000033, 0x000022, 0x000011 };
+AlaPalette crazyb = { 29, crazyb_ };
+AlaColor crazyr_[29] = { 0x001100, 0x002200, 0x003300, 0x004400, 0x005500, 0x006600, 0x007700, 0x008800, 0x009900, 0x00AA00, 0x00BB00, 0x00CC00, 0x00DD00, 0x00EE00, 0x00FF00, 0x00EE00, 0x00DD00, 0x00CC00, 0x00BB00, 0x00AA00, 0x009900, 0x008800, 0x007700, 0x006600, 0x005500, 0x004400, 0x003300, 0x002200, 0x001100 };
+AlaPalette crazyr = { 29, crazyr_ };
+AlaColor crazyg_[29] = { 0x110000, 0x220000, 0x330000, 0x440000, 0x550000, 0x660000, 0x770000, 0x880000, 0x990000, 0xAA0000, 0xBB0000, 0xCC0000, 0xDD0000, 0xEE0000, 0xFF0000, 0xEE0000, 0xDD0000, 0xCC0000, 0xBB0000, 0xAA0000, 0x990000, 0x880000, 0x770000, 0x660000, 0x550000, 0x440000, 0x330000, 0x220000, 0x110000 };
+AlaPalette crazyg = { 29, crazyg_ };
+AlaColor crazy_[32] = { 0x110000, 0x220000, 0x330000, 0x440000, 0x550000, 0x660000, 0x770000, 0x880000, 0x990000, 0xAA0000, 0xBB0000, 0xCC0000, 0xDD0000, 0xEE0000, 0xFF0000, 0x0000FF, 0x00FF00, 0xFF00FF, 0xEE0000, 0xDD0000, 0xCC0000, 0xBB0000, 0xAA0000, 0x990000, 0x880000, 0x770000, 0x660000, 0x550000, 0x440000, 0x330000, 0x220000, 0x110000 };
+AlaPalette crazy = { 32, crazy_ };
+AlaColor randcol_[2] = { random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF) };
+AlaPalette randcol = { 2, randcol_ };
+AlaColor randcoll_[4] = { random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF) };
+AlaPalette randcoll = { 4, randcoll_ };
+AlaColor randcool_[16] = { random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF) };
+AlaPalette randcool = { 16, randcool_ };
 
 
 int animList[33] = {
@@ -65,7 +79,7 @@ int animList[33] = {
   ALA_FADECOLORSLOOP
 };
 
-AlaPalette paletteList[11] = {
+AlaPalette paletteList[18] = {
   alaPalRgb,
   alaPalRainbow,
   alaPalFire,
@@ -76,23 +90,33 @@ AlaPalette paletteList[11] = {
   blancs,
   bleus,
   verts,
-  mauves
+  mauves,
+  crazyb,
+  crazyr,
+  crazy,
+  randcol,
+  randcoll,
+  randcool,
+  crazyg
 };
 
-int durationList[5] = {
+long int durationList[6] = {
   1000,
   10000,
   3000,
   6000,
-  4000
+  4000,
+  random(500, 10000)
 };
 
 
 void setup()
 {
   //delay(4000);
-  rgbStrip.initWS2812(18, 6);
+  rgbStrip.initWS2812(16, 6);
   pinMode(tiltSensorPin, INPUT);
+  pinMode(7, OUTPUT);
+  digitalWrite(7, 1);
   updateAnimation();
 }
 
@@ -107,8 +131,38 @@ void loop()
       duration = random (0, 200);
       updateAnimation();
       faite = 1;
+      AlaColor randcool_[16] = { random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF), random(0x000000, 0xFFFFFF) };
+      AlaPalette randcool = { 16, randcool_ };
+      AlaPalette paletteList[18] = {
+        alaPalRgb,
+        alaPalRainbow,
+        alaPalFire,
+        alaPalRainbowStripe,
+        alaPalParty,
+        alaPalHeat,
+        mycolors,
+        blancs,
+        bleus,
+        verts,
+        mauves,
+        crazyb,
+        crazyr,
+        crazy,
+        randcol,
+        randcoll,
+        randcool,
+        crazyg
+      };
+      long int durationList[6] = {
+        1000,
+        10000,
+        3000,
+        6000,
+        4000,
+        random(500, 10000)
+      };
     }
-    if (tiltSensorState != LOW){
+    if (tiltSensorState != LOW) {
       faite = 0;
     }
   }
@@ -117,5 +171,5 @@ void loop()
 
 void updateAnimation()
 {
-  rgbStrip.setAnimation(animList[animation % 33], durationList[duration % 5], paletteList[palette % 11]);
+  rgbStrip.setAnimation(animList[animation % 33], durationList[duration % 6], paletteList[palette % 18]);
 }
